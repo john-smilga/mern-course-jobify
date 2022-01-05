627c3cbf0eb2302201533ecb0d1f583d04f0538b
john smilga <johnsmilga@gmail.com>
2022-01-04 17:03:42 -0800

    removed client build

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV !== 'production') {
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // only when ready to deploy
-app.use(express.static(path.resolve(__dirname, './client/build')))
+// app.use(express.static(path.resolve(__dirname, './client/build')))
 
 app.use(express.json())
 app.use(helmet())
@@ -44,9 +44,9 @@ app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 // only when ready to deploy
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+// })
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)

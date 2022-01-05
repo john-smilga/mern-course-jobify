bcc2173c0a8eeb6a49868c091504ddd599bb357c
john smilga <johnsmilga@gmail.com>
2022-01-04 16:50:37 -0800

    testing netlify build fix

--- client/src/pages/Dashboard/index.js
+++
@@ -1,6 +0,0 @@
-import AddJob from './AddJob'
-import AllJobs from './AllJobs'
-import Profile from './Profile'
-import SharedLayout from './SharedLayout'
-import Stats from './Stats'
-export { AllJobs, Profile, SharedLayout, Stats, AddJob }

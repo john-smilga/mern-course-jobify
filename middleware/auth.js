dd0b3a1023c9e1b40ebe3b498aeefc31a651a670
Peyman Ahmadi <peyman.ahmadi@outlook.com>
2022-08-25 11:58:24 +0430

    Update auth.js
    probably forgot to remove "UnAuthenticatedError" at line 4.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken'
 import { UnAuthenticatedError } from '../errors/index.js'
 
-UnAuthenticatedError
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization
   if (!authHeader || !authHeader.startsWith('Bearer')) {
